c27062f8cca53ab3a07605f2497a64b5a96b477b
Ruilx <799382072@qq.com>
2016-05-04 00:58:04 +0800

    完善输入流向程序Stdin中,增加一个duringTime(以后可能使用cpuMonitor代替并可以检测出代码是否需要人为检查,即程序执行了核心态语句),增加一个输出流大小超过一定值会返回错误的接口

---
+++ test/a+b_problem.c
@@ -0,0 +1,24 @@
+/***************************************
+** >MODULE NAME< Module
+** Made By Ruilx
+****************************************
+** Title:          >PUT TITLE HERE<
+** Time:           >Year</>Month</>Day<
+** Author:         Ruilx
+** Organization:   GT-Soft Studio
+** License:        GPL
+**
+** Target:         >PUT TARGET HERE<
+** Version:        0.1.0
+****************************************
+** Copyright(C) GT-Soft Studio
+**                  All Rights Reserved.
+***************************************/
+#include <stdio.h>
+
+int main(void){
+	int a, b;
+	scanf("%d%d", &a, &b);
+	printf("%d", a+b);
+	return 0;
+}
---
+++ test/helloworld.c
@@ -0,0 +1,23 @@
+/***************************************
+** >MODULE NAME< Module
+** Made By Ruilx
+****************************************
+** Title:          >PUT TITLE HERE<
+** Time:           >Year</>Month</>Day<
+** Author:         Ruilx
+** Organization:   GT-Soft Studio
+** License:        GPL
+**
+** Target:         >PUT TARGET HERE<
+** Version:        0.1.0
+****************************************
+** Copyright(C) GT-Soft Studio
+**                  All Rights Reserved.
+***************************************/
+
+#include <stdio.h>
+
+int main(void){
+	printf("Hello, world!");
+	return 1;
+}
